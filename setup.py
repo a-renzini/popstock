9533ce1fc6c27c871e2fea53c24ac33e53823330
Arianna Renzini <arianna.renzini@ligo.org>
2024-03-04 11:21:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ def readfile(filename):
     return filecontents
 
 
-long_description = get_long_description()
-
 setup(
     name="pygwb",
     description="A lightweight package for quick evaluation of the stochastic GW energy density Omega_GW emitted by a population of binary black holes and/or neutron stars.",
