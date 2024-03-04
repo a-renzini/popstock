6fbfc67b9ca957da4c6275ed78c4c498d2ae56a8
Arianna Renzini <arianna.renzini@ligo.org>
2024-03-04 03:14:37 -0800

    adding CI and docs

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+
+# get version info from __init__.py
+def readfile(filename):
+    with open(filename) as fp:
+        filecontents = fp.read()
+    return filecontents
+
+
+long_description = get_long_description()
+
+setup(
+    name="pygwb",
+    description="A lightweight package for quick evaluation of the stochastic GW energy density Omega_GW emitted by a population of binary black holes and/or neutron stars."
+    url="https://git.ligo.org/arianna.renzini/popstock",
+    author="Arianna Renzini, Jacob Golomb",
+    author_email="aria.renzini@gmail.com",
+    license="MIT",
+    packages=["popstock"],
+    package_dir={"popstock": "popstock"},
+    scripts=[
+        "population_O3.py",
+    ],
+    install_requires=[
+        "numpy",
+        "scipy>=1.8.0",
+        "bilby>=1.4",
+        "gwpy>=3.0.4",
+        "astropy>=5.2",
+        "gwpopulation",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
